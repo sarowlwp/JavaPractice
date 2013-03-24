1bc11db92c5cda4955d925b3de95e6e045bf224d
wenpingliu <wenpingliu@sohu-inc.com>
2013-03-24 13:57:52 +0800

    add merge sort
    add

--- src/com/sohu/suc/algorithm/sort/BubbleSortDemo.java
+++ src/com/sohu/suc/algorithm/sort/BubbleSortDemo.java
@@ -9,15 +9,16 @@ public class BubbleSortDemo extends BasicDemo {
 	 */
 	@Test
 	public void test(){
-		//loop all
-		for(int i=0 ; i<basicNumber.length;i++){
-			for(int j=0;j<basicNumber.length-1;j++){
-				if(basicNumber[j] > basicNumber[j+1]){
-					swapArrayByIndex(j,j+1);
-				}
-			}
-			printArray(basicNumber);
-		}
+		MergeSort(0,basicNumber.length - 1);
+	}
+
+	private int[] MergeSort(int start, int end) {
+		if(end <= start);
+			return new int[1];
+//		if(end - start == 1){
+//			return basicNumber[end];
+//		}
+		
 	}
 
 }
---
+++ src/com/sohu/suc/algorithm/sort/MergeSortDemo.java
@@ -0,0 +1,23 @@
+package com.sohu.suc.algorithm.sort;
+
+import org.junit.Test;
+
+public class MergeSortDemo extends BasicDemo {
+	
+	/**
+	 * swap
+	 */
+	@Test
+	public void test(){
+		//loop all
+		for(int i=0 ; i<basicNumber.length;i++){
+			for(int j=0;j<basicNumber.length-1;j++){
+				if(basicNumber[j] > basicNumber[j+1]){
+					swapArrayByIndex(j,j+1);
+				}
+			}
+			printArray(basicNumber);
+		}
+	}
+
+}
