c7fd491de51674f1ba717cbd401571b12d48e18e
wenpingliu <wenpingliu@sohu-inc.com>
2013-07-22 23:37:33 +0800

    add sychronzied volatile test

---
+++ src/com/sohu/suc/javatest/StringTest.java
@@ -0,0 +1,15 @@
+package com.sohu.suc.javatest;
+
+public class StringTest {
+	public static void main(String[] args) {
+		String a = "abc";
+		String b = "abcd";
+		String c = "abcd";
+		Integer d = 10;
+		System.out.println(a==b);
+		System.out.println(a.equals(a));
+		System.out.println(a.equals(b));
+		System.out.println(b==c);
+		
+	}
+}
